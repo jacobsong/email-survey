6e8b2db90c3dccbbd77b7f58f91885bf39f7ff3c
Jacob Song <jsong@aqsongjm01.rsn.aac.usarec.army.mil>
2018-06-07 13:54:13 -0400

    added header

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(
+  reducers,
+  {},
+  composeWithDevTools(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
